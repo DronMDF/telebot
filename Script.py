0bc6e9bf523ec0f7be92c1d0d578bb02c92c52d2
Andrey Valyaev <dron.valyaev@gmail.com>
2017-10-12 12:42:54 +0300

    #46: Чистка проекта от старых модулей

--- Script.py
+++
@@ -1,3 +0,0 @@
-def start_sys_service():
-    print("Test")
-    # todo
--- timeOut.py
+++
@@ -1,9 +0,0 @@
-
-import time
-from datetime import datetime
-
-n = datetime.now() #инициализировать в начале программы
-
-time.sleep(100) #тест
-
-print(datetime.now()-n) #вызывать по надобности
