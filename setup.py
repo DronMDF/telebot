32712e83b6f8337fd46c151eb3fb78d7e76a554d
Andrey Valyaev <dron.valyaev@gmail.com>
2017-10-18 22:40:13 +0300

    Разделил style и test проверки

--- setup.py
+++ setup.py
@@ -6,6 +6,25 @@ from sys import exit
 from unittest import TestLoader, TextTestRunner
 
 
+class Style(Command):
+	user_options = []
+
+	def initialize_options(self):
+		pass
+
+	def finalize_options(self):
+		pass
+
+	def run(self):
+		try:
+			if system('find ./ -name "*.py" | xargs pep8 --ignore=W191'):
+				print("Style check failed")
+				exit(-1)
+		except Exception as e:
+			print(e)
+			exit(-1)
+
+
 class Test(Command):
 	user_options = []
 
@@ -17,22 +36,15 @@ class Test(Command):
 
 	def run(self):
 		try:
-			self.style()
-			self.unittest()
+			suite = TestLoader().discover('.', '*.py')
+			runner = TextTestRunner()
+			results = runner.run(suite)
+			if not results.wasSuccessful():
+				print("Test failed")
+				exit(-1)
 		except Exception as e:
 			print(e)
 			exit(-1)
-
-	def style(self):
-		if system('find ./ -name "*.py" | xargs pep8 --ignore=W191'):
-			raise RuntimeError("Style check failed")
-
-	def unittest(unittest):
-		suite = TestLoader().discover('.', '*.py')
-		runner = TextTestRunner()
-		results = runner.run(suite)
-		if not results.wasSuccessful():
-			raise RuntimeError("Test failed")
 
 
 if __name__ == '__main__':
@@ -45,5 +57,5 @@ if __name__ == '__main__':
 		url='https://github.com/DronMDF/telebot',
 		packages=['tb', 'tb.sources', 'tb.storage'],
 		data_files=[('/usr/bin/telebot', ['telebot'])],
-		cmdclass={'test': Test}
+		cmdclass={'style': Style, 'test': Test}
 	)
