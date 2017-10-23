a6ef52edbf25f3c73dd997a736ef886a1f5afba5
Andrey Valyaev <dron.valyaev@gmail.com>
2017-10-23 23:13:03 +0300

    Update syslog.py

--- tb/sources/syslog.py
+++ tb/sources/syslog.py
@@ -2,6 +2,6 @@ class SoSyslog:
 	def actions(self):
 		sys_history = " "
 		with open('./messages.txt') as syslogFile:
-		for line in syslogFile:
-			sys_history += str(line)
+			for line in syslogFile:
+				sys_history += str(line)
 		return sys_history
