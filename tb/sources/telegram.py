53d760ecb32e11873ddf46e1dafca5a8abae63a2
Andrey Valyaev <dron.valyaev@gmail.com>
2017-10-23 23:10:57 +0300

    Update telegram.py
    Здесь пока не хватает AcMessage, но с ним я разберусь чуть позже.

--- tb/sources/telegram.py
+++ tb/sources/telegram.py
@@ -1,10 +1,10 @@
+import telegram
+
 class SoTelegram:
 	def __init__(self, config):
-		self.token = config.value('telegram.token')
-               
+		self.config = config
 
 	def actions(self):
-                self.config = config
-                bot = telegram.Bot(config.value("telegram.token"))
-                update = bot.getUpdate()
-                return[AcMessage(u) for u in update]
+		bot = telegram.Bot(config.value("telegram.token"))
+		update = bot.getUpdate()
+		return [AcMessage(u) for u in update]
