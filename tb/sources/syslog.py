9967688ac83e5843034bdf3bccd89443b62a5689
sminux <espin55@yandex.ru>
2017-10-23 21:27:07 +0300

    #43 bot.getUpdates #43 syslog messages

--- tb/sources/syslog.py
+++ tb/sources/syslog.py
@@ -1,7 +1,8 @@
 class SoSyslog:
 	def actions(self):
-		# @todo #40 Необходимо получить список событий, которые
-		#  возникли с момента последнего обращения. Не знаю точно, что
-		#  мы хотим там увидеть, Давайте для начала найдет все события
-		#  от systemd и вернем их в виде событий.
-		return []
+                sys_history = " "
+		with open('./messages.txt') as syslogFile:
+                        for line in syslogFile:
+                            sys_history += str(line)
+                return sys_history
+		
