652f2c64a24effd3c91a36f648720f50c17570e2
Andrey Valyaev <dron.valyaev@gmail.com>
2017-10-25 16:20:06 +0300

    #39: Отправка сообщений на сервер telegram

--- tb/sources/telegram.py
+++ tb/sources/telegram.py
@@ -1,11 +1,25 @@
 import telegram
 
 
+# @todo #39 Перенести AcMessage в правильное место
+class AcTelegramUpdate:
+	def __init__(self, update):
+		self.update = update
+
+	def json(self):
+		return {
+			'chat_id': self.update.message.chat.id,
+			'text': self.update.message.text,
+		}
+
+
 class SoTelegram:
 	def __init__(self, config):
 		self.config = config
 
 	def actions(self):
-		bot = telegram.Bot(config.value("telegram.token"))
-		update = bot.getUpdate()
-		return [AcMessage(u) for u in update]
+		bot = telegram.Bot(self.config.value("telegram.token"))
+		# @todo #39 getUpdates нужно вызывать с номером очередного сообщения.
+		#  А пока он повторяет все сообщения, которые к нему поступили.
+		update = bot.getUpdates()
+		return [AcTelegramUpdate(u) for u in update]
--- tb/storage/telegram.py
+++ tb/storage/telegram.py
@@ -1,11 +1,10 @@
+import telegram
+
+
 class StTelegram:
 	def __init__(self, config):
-		self.token = config.value('telegram.token')
+		self.config = config
 
 	def save(self, action):
-		# @todo #35 Нужно обратиться к серверу telegram
-		#  и залить туда. Для этого нужно использовать
-		#  https://python-telegram-bot.org/ Но пример неправильный,
-		#  поскольку нам нужно только отправить сообщения на сервер.
-		#  что-то типа этого: telegram.Bot.sendMessage
-		pass
+		bot = telegram.Bot(self.config.value('telegram.token'))
+		bot.sendMessage(**action.json())
