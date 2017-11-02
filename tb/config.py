c9190488a3bee27efc7a900ecaeed62f3223734f
Andrey Valyaev <dron.valyaev@gmail.com>
2017-11-02 13:40:22 +0300

    #59: Чтение файла конфигурации

--- tb/config.py
+++ tb/config.py
@@ -1,4 +1,5 @@
 from argparse import ArgumentParser
+from configparser import ConfigParser, NoOptionError, NoSectionError
 from unittest import TestCase
 
 
@@ -12,16 +13,60 @@ class ConfigDefault:
 		return self.values[name]
 
 
+class ConfigFile:
+	''' Файл с конфигурацией '''
+	def __init__(self, config):
+		self.config = config
+
+	def readall(self):
+		return open(self.config.value('config'), 'r').readall()
+
+
+class ConfigString:
+	''' Строка с файловой конфигурацией '''
+	def __init__(self, content):
+		self.content = content
+
+	def readall(self):
+		return self.content
+
+
 class ConfigFromFile:
-	# @todo #39 Необходимо организовать чтение конфигурации
-	#  из файла конфигурации. Формат файла конфигурации - ini
-	#  модуль: https://docs.python.org/3/library/configparser.html
-	def __init__(self, filename, defaults):
-		self.filename = filename
+	def __init__(self, configfile, defaults):
+		self.configfile = configfile
 		self.defaults = defaults
 
 	def value(self, name):
-		raise RuntimeError("Wrong configuration value name")
+		config = ConfigParser()
+		config.read_string(self.configfile.readall())
+		try:
+			result = config.get(*name.split('.'))
+		except (NoSectionError, NoOptionError):
+			result = self.defaults.value(name)
+		return result
+
+
+class ConfigFromFileTest(TestCase):
+	def testTelegramTokenInFile(self):
+		config = ConfigFromFile(
+			ConfigString('[telegram]\ntoken=token'),
+			ConfigDefault({})
+		)
+		self.assertEqual(config.value('telegram.token'), 'token')
+
+	def testConfigFileDefaults(self):
+		config = ConfigFromFile(
+			ConfigString(''),
+			ConfigDefault({'section.key': 'value'})
+		)
+		self.assertEqual(config.value('section.key'), 'value')
+
+	def testConfigFileDefaultsIfSectionPresent(self):
+		config = ConfigFromFile(
+			ConfigString('[section]'),
+			ConfigDefault({'section.key': 'value'})
+		)
+		self.assertEqual(config.value('section.key'), 'value')
 
 
 class ConfigFromArgs:
