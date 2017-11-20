8a9bfb1212109ec7dded5f40fe610c5b55709e38
sminux <espin55@yandex.ru>
2017-11-20 18:52:28 +0300

    #56: получение номера последнего сообщения

--- tb/storage/telegram.py
+++ tb/storage/telegram.py
@@ -15,8 +15,4 @@ class StDbTelegramOffset:
 		self.db = db
 
 	def save(self, action):
-		# @todo #39 Необходимо сохранить в БД номер сообщения,
-		#  с которым работал бот. Это необходимо для того, чтобы после
-		#  перезапуска бот не повторял свои сообщения. Можно использовать
-		#  https://pypi.python.org/pypi/tinydb - достаточно простая штука.
-		pass
+		self.db.set('update_id', message.json('update_id'))
