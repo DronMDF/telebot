7a8f92271e9e5940d79178cb70b97a0286860ae2
Michaelo0 <nadnedmad@gmail.com>
2017-11-27 19:11:00 +0300

    style fixes

--- tb/sources/reaction.py
+++ tb/sources/reaction.py
@@ -17,14 +17,16 @@ class ReactionEcho:
 	def react(self, update):
 		return AcTelegramUpdate(update)
 
-class ReactionChoiced:
-	def __init__(self,*reactions):
-		self.reactions=reactions
 
-	def check(self,update):
+class ReactionChoiced:
+	def __init__(self, *reactions):
+		self.reactions = reactions
+
+	def check(self, update):
 		return True
 
-	def react(self,update):
+	def react(self, update):
 		for r in self.reactions:
 			if r.check(update):
 				return r.react(update)
+			
