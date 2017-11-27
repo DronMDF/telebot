a0737130b47fdf1a22cbdec7e6c97aaa5c77cc9b
Andrey Valyaev <dron.valyaev@gmail.com>
2017-11-27 22:33:21 +0300

    #41: Забыл self

--- tb/sources/hdd.py
+++ tb/sources/hdd.py
@@ -1,7 +1,7 @@
 import psutil
 
 class SoHDD:
-	def actions():
+	def actions(self):
 		if psutil.disk_usage('/').percent > 90:
 			# @todo #41 Сформировать Action, который доставит админу
 			#  уведомление о том, что на диске кончилось место
