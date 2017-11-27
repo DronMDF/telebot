af24b2c65b35dadb1ec88a9ff98562dbed74b026
Andrey Valyaev <dron.valyaev@gmail.com>
2017-11-27 22:45:45 +0300

    #48: Ситлистические исправления и todo

--- tb/sources/utility.py
+++ tb/sources/utility.py
@@ -1,11 +1,19 @@
 import itertools
 
+
 class SoJoin:
 	def __init__(self, *sources):
 		self.sources = sources
 
 	def actions(self):
 		return list[itertools.chain(s.actions() for s in self.sources)]
+	
+	
+class SoJoinTest:
+	# @todo Что-то я немного сомневаюсь, что SoJoin работает корректно.
+	#  И это удобное место, чтобы попробовать написать тест.
+	#  Примеры оформления тестов можно посмотреть в других файлах.
+	pass
 
 
 class SoSafe:
