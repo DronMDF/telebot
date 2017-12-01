3dfbf4b375621e438d5d899c72e3a816e3f1019e
Andrey Valyaev <dron.valyaev@gmail.com>
2017-12-01 23:47:25 +0300

    #87: Конкретная реакция

--- tb/sources/__init__.py
+++ tb/sources/__init__.py
@@ -1,6 +1,6 @@
 from .cpu import SoCPU
 from .hdd import SoHDD
-from .reaction import ReactionEcho
+from .reaction import ReactionEcho, ReactionConcrete, ReactionChoiced
 from .syslog import SoSyslog
 from .telegram import SoTelegram, TelegramBot, TelegramOffsetFromDb
 from .utility import SoJoin, SoSafe
