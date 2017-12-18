f78a691a45fe101b8395390394504b1b71012e24
Andrey Valyaev <dron.valyaev@gmail.com>
2017-12-18 23:21:43 +0300

    Update hdd.py

--- tb/sources/hdd.py
+++ tb/sources/hdd.py
@@ -3,23 +3,23 @@ import psutil
 
 class AcLowHDD:
         def __init__(self, chat_id):
-            self.chat_id = chat_id
+                self.chat_id = chat_id
 
         def save(self, db):
-            pass
+                pass
 
         def send(self, transport):
-            transport.sendMessage(
-                chat_id=self.chat_id,
-                text="Мало места на жестком диске"
-            )
+                transport.sendMessage(
+                        chat_id=self.chat_id,
+                        text="Мало места на жестком диске"
+                )
 
 
 class SoHDD:
-    def __init__(self, chat_id):
-        self.chat_id = chat_id
-        
-    def actions(self):
-        if psutil.disk_usage('/').percent > 90:
-           return[AcLowHDD(self.chat_id)]
-        return []
+        def __init__(self, chat_id):
+                self.chat_id = chat_id
+
+        def actions(self):
+                if psutil.disk_usage('/').percent > 90:
+                        return [AcLowHDD(self.chat_id)]
+                return []
